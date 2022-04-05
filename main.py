2377e8c2e757efd64751f8d31cab126674cf6ddc
remok <remok@student.ethz.ch>
2022-04-06 00:24:33 +0200

    Add simple Unet

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from src.data.dataset import RoadSegDataModule
-from src.models.UNet import CustomUNet
+from src.models.UNet import PretrainedUNet, SMPModel
+from segmentation_models_pytorch.encoders import get_preprocessing_fn
 
-
-MODEL_DICT = {
-    "baseline_unet": CustomUNet
-}
 
 def run_experiment():
     seed_everything(1234)
+    encoder = "resnet34"
+    encoder_weights = "imagenet"
 
-    datamodule = RoadSegDataModule(data_dir="data")
-    model = CustomUNet()
+    datamodule = RoadSegDataModule(
+        data_dir="data")
+    model = SMPModel("Unet", encoder, 3, 1)
     logger = TensorBoardLogger(save_dir="logs")
 
-    trainer = Trainer(max_epochs=50, logger=logger)
+    trainer = Trainer(max_epochs=5, logger=logger, log_every_n_steps=5)
     trainer.fit(model, datamodule=datamodule)
 
 
--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,28 +2,41 @@ import os
 
 import albumentations as A
 import cv2
-import zipfile
 import numpy as np
 import pytorch_lightning as pl
 from albumentations.pytorch import ToTensorV2
 from torch.utils.data import DataLoader, Dataset
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.sampler import SequentialSampler
+
+
+def one_hot_encode(mask, class_colors):
+    num_classes = len(class_colors)
+    one_hot = np.zeros((mask.shape[0], mask.shape[1], num_classes))
+    for i, color in enumerate(class_colors):
+        one_hot[:, :, i][mask == color] = 1
+    return one_hot
+
+
+def one_hot_decode(one_hot, class_colors):
+    x = np.argmax(one_hot, axis=0)
+    for i, color in enumerate(class_colors):
+        x[x == i] = color
+    return x
 
 
 class RoadSegDataset(Dataset):
-    def __init__(self, img_path, mask_path=None, transform=None):
+    def __init__(self, img_path, mask_path, transform=None, preprocessing=None):
         self.img_path = img_path
         self.mask_path = mask_path
         self.transform = transform
+        self.preprocessing = preprocessing
         self.img_names = os.listdir(img_path)
-
-    def wrapped_transform(self, image, mask):
-        if image is not None and mask is not None:
-            return self.transform(image=image, mask=mask)
-        if image is not None:
-            return self.transform(image=image)
-        else:
-            raise AttributeError("No image data available")
+    
+    def _preprocess_mask(self, mask):
+        mask = mask.astype(np.float32)
+        mask[mask == 255] = 1.0
+        mask = mask[..., np.newaxis]
+        return mask
 
     def __len__(self):
         return len(self.img_names)
@@ -33,72 +46,98 @@ class RoadSegDataset(Dataset):
 
         image = cv2.imread(os.path.join(self.img_path, img_name))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        if self.mask_path:
-            mask = cv2.imread(os.path.join(self.mask_path, img_name), cv2.IMREAD_UNCHANGED)
-            mask //= 255
+
+        mask = cv2.imread(os.path.join(
+            self.mask_path, img_name), cv2.IMREAD_UNCHANGED)
+        mask = self._preprocess_mask(mask)
 
         if self.transform:
-            sample = self.wrapped_transform(image, mask)
-            image, mask = sample.get("image"), sample.get("mask")
+            sample = self.transform(image=image, mask=mask)
+            image, mask = sample["image"], sample["mask"]
+
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample["image"], sample["mask"]
+
         return image, mask
 
 
+class RoadSegTestDataset(Dataset):
+    def __init__(self, img_path, transform=None, preprocessing=None):
+        self.img_path = img_path
+        self.transform = transform
+        self.preprocessing = preprocessing
+        self.img_names = os.listdir(img_path)
+
+    def __len__(self):
+        return len(self.img_names)
+
+    def __getitem__(self, index):
+        img_name = self.img_names[index]
+
+        image = cv2.imread(os.path.join(self.img_path, img_name))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transform:
+            sample = self.transform(image=image)
+            image = sample["image"]
+
+        if self.preprocessing:
+            sample = self.preprocessing(image=image)
+            image = sample["image"]
+
+        return image
+
+
 class RoadSegDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir="../../data", batch_size=8, val_split=0.1, shuffle=True, random_seed=42, num_workers=4):
+    def __init__(self, data_dir="../../data", batch_size=8, val_split=0.1, shuffle=True, random_seed=42, num_workers=4, preprocessing_fn=None):
         self.batch_size = batch_size
         self.val_split = val_split
         self.shuffle = shuffle
         self.random_seed = random_seed
         self.num_workers = num_workers
         self.data_dir = data_dir
+        self.preprocessing_fn = preprocessing_fn
 
-        self.train_img_path = os.path.join(data_dir, "processed/training/images")
-        self.train_mask_path = os.path.join(data_dir, "processed/training/groundtruth")
+        self.train_img_path = os.path.join(
+            data_dir, "processed/training/images")
+        self.train_mask_path = os.path.join(
+            data_dir, "processed/training/groundtruth")
         self.test_img_path = os.path.join(data_dir, "processed/test/images")
 
         self.train_transform = A.Compose(
             [
-                A.ShiftScaleRotate(
-                    shift_limit=0.2, scale_limit=0.2, rotate_limit=30, p=0.5),
-                A.RGBShift(r_shift_limit=25, g_shift_limit=25,
-                           b_shift_limit=25, p=0.5),
-                A.RandomBrightnessContrast(
-                    brightness_limit=0.3, contrast_limit=0.3, p=0.5),
-                A.Normalize(mean=(0.485, 0.456, 0.406),
-                            std=(0.229, 0.224, 0.225)),
-                ToTensorV2(transpose_mask=True),
+                A.Resize(384, 384),
+                A.OneOf(
+                    [
+                        A.HorizontalFlip(p=1),
+                        A.VerticalFlip(p=1),
+                        A.RandomRotate90(p=1),
+                    ],
+                    p=0.75,
+                )
             ]
         )
-        self.val_transform = A.Compose(
-            [
-                A.Normalize(mean=(0.485, 0.456, 0.406),
-                            std=(0.229, 0.224, 0.225)),
-                ToTensorV2(transpose_mask=True),
-            ]
-        )
+
         self.test_transform = A.Compose(
             [
-                A.Normalize(mean=(0.485, 0.456, 0.406),
-                            std=(0.229, 0.224, 0.225)),
-                ToTensorV2(transpose_mask=True),
+                A.Resize(384, 384)
             ]
         )
 
-    def prepare_data(self):
-        processed_dir = os.path.join(self.data_dir, "processed")
-        raw_dir = os.path.join(self.data_dir, "raw")
-        if not os.path.isdir(processed_dir):
-            zip_file = os.path.join(raw_dir, "cil-road-segmentation-2022.zip")
-            with zipfile.ZipFile(zip_file, 'r') as f:
-                f.extractall(processed_dir)
-
     def setup(self, stage=None):
+        preprocessing_ = []
+        if self.preprocessing_fn:
+            preprocessing_.append(A.Lambda(image=self.preprocessing_fn))
+        preprocessing_.append(ToTensorV2(transpose_mask=True))
+        preprocessing = A.Compose(preprocessing_)
+
         self.train_dataset = RoadSegDataset(
-            img_path=self.train_img_path, mask_path=self.train_mask_path, transform=self.train_transform)
+            img_path=self.train_img_path, mask_path=self.train_mask_path, transform=self.train_transform, preprocessing=preprocessing)
         self.val_dataset = RoadSegDataset(
-            img_path=self.train_img_path, mask_path=self.train_mask_path, transform=self.val_transform)
-        self.test_dataset = RoadSegDataset(
-            img_path=self.test_img_path, transform=self.test_transform)
+            img_path=self.train_img_path, mask_path=self.train_mask_path, transform=self.test_transform, preprocessing=preprocessing)
+        self.test_dataset = RoadSegTestDataset(
+            img_path=self.test_img_path, transform=self.test_transform, preprocessing=preprocessing)
 
         train_len = len(self.train_dataset)
         idx = list(range(train_len))
@@ -109,8 +148,8 @@ class RoadSegDataModule(pl.LightningDataModule):
             np.random.shuffle(idx)
 
         train_idx, val_idx = idx[split:], idx[:split]
-        self.train_sampler = SubsetRandomSampler(train_idx)
-        self.val_sampler = SubsetRandomSampler(val_idx)
+        self.train_sampler = SequentialSampler(train_idx)
+        self.val_sampler = SequentialSampler(val_idx)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, sampler=self.train_sampler, num_workers=self.num_workers)
@@ -118,5 +157,5 @@ class RoadSegDataModule(pl.LightningDataModule):
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size, sampler=self.val_sampler, num_workers=self.num_workers, shuffle=False)
 
-    def test_dataloader(self):
+    def predict_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
